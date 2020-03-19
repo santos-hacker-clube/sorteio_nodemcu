4e85872eac867a4a4ce66aa74143dec0c53edbb8
piscaleds <43486278+piscaleds@users.noreply.github.com>
2020-03-18 21:19:30 -0300

    Update sorteio_ajax.ino

--- sorteio_ajax.ino
+++ sorteio_ajax.ino
@@ -20,12 +20,13 @@
 #include <ESP8266WebServer.h>
 
 #ifndef APSSID
-#define APSSID "SHC"
-#define APPSK  "sorteio"
+#define APSSID "ESPap"
+#define APPSK  "thereisnospoon"
 #endif
 
 
-#include "index.h" // Incluir o arquivo index.h
+#include "index.h" 
+
 
 
 int botaoSorteio = 0;
@@ -36,50 +37,60 @@ const char *password = APPSK;
 
 ESP8266WebServer server(80);
 
-/* Acesse via navegador http://192.168.4.1
-*/
 void handleRoot() {
-  String s = MAIN_page;
-  server.send(200, "text/html", s); //envia
+ String s = MAIN_page;
+ server.send(200, "text/html", s); 
 }
 
 void handleADC() {
-  int a = digitalRead(botaosorteio);
-  String adcValue = String(a);
-  botaoSorteio = digitalRead(botaosorteio);
-  if ( botaoSorteio == LOW) {
-    randomSorteio = random(0, 100);
+ 
     int x = randomSorteio;
     String sorteioValue = String(x);
-    server.send(200, "text/plane", sorteioValue);
+    server.send(200, "text/plane", sorteioValue); 
     delay (1000);
-  }
-
-
+  
+    
+ 
 }
 
-void setup(void) {
-
+void setup(void){
+  
   pinMode(0, INPUT_PULLUP);
   Serial.begin(115200);
   WiFi.softAP(ssid, password);
   delay(5000);
   Serial.println("");
+  Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP()); 
   IPAddress myIP = WiFi.softAPIP();
   randomSeed(digitalRead(0));
-  server.on("/", handleRoot);
+  server.on("/", handleRoot);      
   server.on("/readADC", handleADC);
-  server.begin();
+  server.begin();                  
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
-  server.handleClient();
+void loop(void){
+  
+  botaoSorteio = digitalRead(botaosorteio);
+  if ( botaoSorteio == HIGH) {
+     
+    delay(10);
 
+  } 
+  
+  else {
+    
+    randomSorteio = random(0, 50);
+    Serial.println(randomSorteio);
+    delay(1000);
+  }
+  
+  server.handleClient();         
+  
 }
 
 /*
